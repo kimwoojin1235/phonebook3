42ef2369af34a41b4d27f9c6d19ecdf8ee98195d
user <user@PC2005221900>
2021-01-19 19:22:32 +0900

    금일 수업+과제

---
+++ src/main/java/com/javaex/controller/PhoneController.java
@@ -0,0 +1,93 @@
+package com.javaex.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.javaex.dao.PhoneDao;
+import com.javaex.vo.PhoneVo;
+
+@Controller
+@RequestMapping(value = "/phone")
+public class PhoneController {
+	// 필드
+
+	// 생성자
+
+	// 메소드g/s
+
+	/** 메소드 일반***메소드 마다 기능 1개씩-->기능마다 url 부여 */
+	// 테스트
+
+	// 리스트
+	@RequestMapping(value = "/list",method = {RequestMethod.GET,RequestMethod.POST})
+	public String List(Model model) {//model은 리스트의 주소를 옮기는 상자이다.
+		System.out.println("list");
+		
+		//dao를 톨해 리스트을 가져온다.
+		PhoneDao phoneDao = new PhoneDao();
+		List<PhoneVo> phoneList=phoneDao.getpersonList();
+		
+		//model-->데이터를 옮기는 방법-->담으면 된다
+		model.addAttribute("plist",phoneList);
+		//모델을 어트리뷰트로 옮긴다.
+		
+		System.out.println(phoneList.toString());
+		
+		return "/WEB-INF/views/list.jsp";
+	}
+
+	// 등록폼
+	@RequestMapping(value = "/writeForm",method = {RequestMethod.GET,RequestMethod.POST})
+	public String writeFoem() {
+		System.out.println("등록폼입니다.");
+		return "/WEB-INF/views/writeForm.jsp";
+		//내부에 파일을 찾는 포워드 방식이다
+	}
+	//http://localhost:8088/phonebook3/phone/write?name=김우진&hp=010-4567-4567&company=02-4567-4567
+	// 등록
+	@RequestMapping(value = "/write",method = {RequestMethod.GET,RequestMethod.POST})
+	public String write(@RequestParam("name") String name, @RequestParam("hp") String hp,
+			@RequestParam("company") String company) {
+		System.out.println("write");
+		PhoneVo phoneVo =new PhoneVo(name,hp,company);
+		System.out.println(phoneVo.toString());
+		PhoneDao phoneDao = new PhoneDao();
+		phoneDao.personInsert(phoneVo);
+		
+		return "redirect:/phone/list";
+		//이주소로 보내라는 리다이렉트 방식이다.
+	}
+	// 수정폼-->modifyForm
+	@RequestMapping(value = "/modifyForm",method = {RequestMethod.GET,RequestMethod.POST})
+	public String modifyForm(Model model,@RequestParam("personid") int personid) {
+		System.out.println("수정폼입니다.");
+		PhoneDao phoneDao = new PhoneDao();
+		PhoneVo pVo =phoneDao.getPerson(personid);
+		model.addAttribute("personVo",pVo);
+		return "/WEB-INF/views/updateForm.jsp";
+	}
+	// 수정-->modify
+	@RequestMapping(value = "/modify",method = {RequestMethod.GET,RequestMethod.POST})
+	public String modify(@RequestParam("id") int personid, @RequestParam("name") String name, @RequestParam("hp") String hp,
+			@RequestParam("company") String company) {
+		PhoneVo phoneVo = new PhoneVo(personid, name, hp, company);
+		PhoneDao phoneDao =new PhoneDao();
+		phoneDao.personUpdate(phoneVo);
+		
+		
+		return "redirect:/phone/list";
+	}
+	// 삭제-->delete
+	@RequestMapping(value = "/delete",method = {RequestMethod.GET,RequestMethod.POST})
+	public String delete(@RequestParam("id") int id) {
+		System.out.println("삭제입니다.");
+		PhoneDao phoneDao =new PhoneDao();
+		phoneDao.persondelete(id);
+		return "redirect:/phone/list";
+	}
+}
---
+++ webapp/WEB-INF/views/updateForm.jsp
@@ -0,0 +1,21 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h1>김우진 전화번호 수정페이지</h1>
+	<p>수정 화면 입니다.
+	   아래항목을 수정 하시고 "수정" 버튼을 눌러주세요	</p>
+	<Form action="/phonebook3/phone/modify" method="get">
+	이름(name)<input type="text" name="name" value="${personVo.name }"><br>
+	핸드폰(hp)<input type="text" name="hp" value="${personVo.hp }"><br>
+	회사(company)<input type="text" name="company" value="${personVo.company }"><br>
+	코드(id)<input type="text" name="Id" value="${personVo.personid }"readonly><br>
+	<button type="submit">수정</button>
+	</Form>
+</body>
+</html>
